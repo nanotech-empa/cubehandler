4fc96e7abcfe9132ec14d9bd3215857ffd2d250c
Carlo Antonio Pignedoli <c.pignedoli@gmail.com>
2024-04-04 11:47:22 +0200

    reqeusted improvements

--- tests/test_cube.py
+++ tests/test_cube.py
@@ -1,10 +1,10 @@
 import os
-
+from pathlib import Path
 import numpy as np
-
 from cubehandler import Cube
 
-this_dir = os.path.dirname(os.path.abspath(__file__))
+this_file_path = Path(__file__)
+this_dir = this_file_path.parent.absolute()
 
 
 def test_empty_cube():
@@ -13,17 +13,17 @@ def test_empty_cube():
 
 
 def test_read_cube():
-    cube = Cube.from_file(this_dir + "/CH4_HOMO.cube")
+    cube = Cube.from_file(this_dir / "CH4_HOMO.cube")
     assert np.allclose(cube.origin, np.array([1.0, 0, 0]), atol=0.001)
     assert np.allclose(
         cube.cell,
-        np.array([[22.1019, 0, 0], [0, 22.0465, 0], [0, 0, 22.1979]]),
+        np.array([[15.1178, 0, 0], [0, 15.1178, 0], [0, 0, 15.1178]]),
         atol=0.001,
     )
 
 
 def test_get_xyz_index():
-    cube = Cube.from_file(this_dir + "/CH4_HOMO.cube")
+    cube = Cube.from_file(this_dir / "CH4_HOMO.cube")
     assert cube.get_x_index(1) == 2
-    assert cube.get_y_index(1) == 5
-    assert cube.get_z_index(1) == 5
+    assert cube.get_y_index(1) == 4
+    assert cube.get_z_index(1) == 4
