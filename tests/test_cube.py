52fadef9e56418c30b638bf0bbd23e1738a5c57b
Carlo Antonio Pignedoli <c.pignedoli@gmail.com>
2024-04-10 16:47:00 +0200

    bug fix

--- tests/test_cube.py
+++ tests/test_cube.py
@@ -84,10 +84,10 @@ def test_x_arr_ang():
 
 def test_reduce_data_density():
     cube = Cube.from_file(this_dir / "CH4_HOMO.cube")
-    integral = np.sum(cube.data) * cube.dv_au * cube.scaling_f
+    integral = np.sum(cube.data**2) * cube.dv_au * cube.scaling_f
     cube.reduce_data_density(points_per_angstrom=2)
     cube.write_cube_file("low_res.cube", low_precision=True)
     low_res = Cube.from_file("low_res.cube")
-    low_res_integral = np.sum(low_res.data) * low_res.dv_au * low_res.scaling_f
-    # assert np.abs(low_res_integral - integral) < 0.005
+    low_res_integral = np.sum(low_res.data**2) * low_res.dv_au * low_res.scaling_f**2
+    assert np.abs(low_res_integral - integral) < 0.01
     assert cube.scaling_f == 0.2848452
